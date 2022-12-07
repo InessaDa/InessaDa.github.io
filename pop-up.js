0619c58d41370507269d930ac17ecd4d5f8b7711
Inessa <39734534+InessaDa@users.noreply.github.com>
2022-12-08 05:11:08 +1000

    Update pop-up.js

--- pop-up.js
+++ pop-up.js
@@ -4,10 +4,8 @@ var closeBtn = document.querySelector('.pop-up-close')
 
 button.addEventListener("click", function (_event) {
     popUp.classList.remove('pop-up-hidden')
-    document.body.style.position = 'fixed';
   });
 
   closeBtn.addEventListener("click", function (_event) {
     popUp.classList.add('pop-up-hidden')
-    document.body.style.position = '';
   });
